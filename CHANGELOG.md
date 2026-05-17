0b305a569c156be037f9bc003bb83492fad586cd
InetByOu <edisaedi.id@gmail.com>
2026-05-17 12:22:26 +0700

    Clean repository

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-#### Changelog v1.0.0 - 03-04-26
-- Realese Qtun magisk install
-
-# QTUN Tunneling Changelog
-
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-zip -r -o -X QTUN-Magisk-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'CHANGELOG.md' -x 'update.json' -x 'build.sh' -x '.github/*' -x 'QTUN/run/*' -x 'QTUN/bin/.bin' -x '.gitignore'
